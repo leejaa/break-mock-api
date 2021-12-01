0c823d10049f58dcfa57e8d3470fcabd8f193cb3
이자훈 <jh.lee@break.co.kr>
2021-12-01 15:50:55 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f9d13b70f9164be88b2fa35df180c6d5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0af3786-1cdb-4185-852d-57a5f0406c3c'
+};
