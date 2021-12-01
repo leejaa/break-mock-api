15c7ba1f7a1946c971dd84fe99ea11f4fca9da06
이자훈 <jh.lee@break.co.kr>
2021-12-01 17:53:22 +0900

    Update middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@ module.exports = {
       cors: {
         enabled: true, 
         // headers: '*', 
-        origin: ["http://localhost"],
+        origin: ["http://localhost", "http://localhost:3000"],
       },
     },
   };
