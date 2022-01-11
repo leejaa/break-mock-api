3d93894fc0f145dd05984b9d62a922d3d1b5a9cf
이자훈 <jh.lee@break.co.kr>
2022-01-11 12:28:14 +0900

    feat: auth api 추가

--- api/payments/controllers/payments.js
+++ api/payments/controllers/payments.js
@@ -1,3 +1,7 @@
+const { sanitizeEntity } = require('strapi-utils');
+const axios = require('axios');
+
+const secretKey = 'test_sk_jZ61JOxRQVEnkkRa2OyrW0X9bAqw';
 
 module.exports = {
     async find(ctx) {
@@ -11,5 +15,77 @@ module.exports = {
             },
             message: '주문 생성',
         };
+    },
+    async auth(ctx) {
+        axios.post('https://api.tosspayments.com/v1/connectpay/authorizations/access-token', {
+           grantType: 'AuthorizationCode',
+            code: ctx.query.code,
+            customerKey: ctx.query.customerKey,
+        }, {
+            headers: {
+                Authorization: "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+                "Content-Type": "application/json",     
+            }
+        }).then((response) => {
+            console.log('response', response.data);
+            const { accessToken, refreshToken, tokenType, expiresIn } = response.data;
+            return {
+                code: 200,
+                data: {
+                    id: 5012,
+                },
+                message: '인증',
+            };
+        }).catch((error) => {
+            console.log('error', error);
+            return {
+                code: 200,
+                data: {
+                    id: 5012,
+                },
+                message: '인증',
+            };
+        })
+        return {
+            code: 200,
+            data: {
+                id: 5012,
+            },
+            message: '인증',
+        };
+    //     got
+    // .post("https://api.tosspayments.com/v1/connectpay/authorizations/access-token", {
+    //   headers: {
+    //     Authorization:
+    //       "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+    //     "Content-Type": "application/json",
+    //   },
+    //   json: {
+    //     grantType: 'AuthorizationCode',
+    //     code: ctx.query.code,
+    //     customerKey: ctx.query.customerKey,
+    //   },
+    //   responseType: "json",
+    // })
+    // .then(function (response) {
+    //     console.log('response', response);
+    //     return {
+    //         code: 200,
+    //         data: {
+    //             id: 5012,
+    //         },
+    //         message: '인증',
+    //     };
+    // })
+    // .catch(function (error) {
+    //     console.log('error', error);
+    //     return {
+    //         code: 200,
+    //         data: {
+    //             id: 5012,
+    //         },
+    //         message: '인증',
+    //     };
+    // });
     },
 };
--- config/middleware.js
+++ config/middleware.js
@@ -2,7 +2,8 @@ module.exports = {
     settings: {
       cors: {
         enabled: true,
-        origin: ['*']
+        // origin: ['*']
+        origin: ['http://localhost:3000', 'http://localhost:1337', 'https://staging-brg.break.co.kr', 'https://staging-brg3.break.co.kr']
       },
     },
   };
